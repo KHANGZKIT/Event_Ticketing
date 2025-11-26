ac06c9196c34bb2f2492aa70731a905b09a1da41
KHANGZKIT <vutienkhang01@gmail.com>
2025-11-26 11:17:40 +0700

    fix: Add config

--- services/events/src/modules/events/events.routes.js
+++ services/events/src/modules/events/events.routes.js
@@ -6,11 +6,9 @@ const r = Router();
 
 r.get('/', ctrl.listEvents); //list Events
 r.get('/:id', ctrl.getEvent);   // Lay event theo id
-
 r.post('/', authGuard, requireRole('admin'), ctrl.createEvent); // Tao su kien  
 r.patch('/:id', authGuard, requireRole('admin'), ctrl.updateEvent); // Cap Nhat su  kien
 r.delete('/:id', authGuard, requireRole('admin'), ctrl.deleteEvent); //Xoa su kien
-
 r.get('/:id/shows', ctrl.listShowsOfEvent);
 
 export default r;
